ff2c6b72870fb5f00059800c19ac707b066e8814
Léo Saad <leosaad36@gmail.com>
2024-07-22 01:38:10 -0300

    novos audio + segredo

--- luiz/script.js
+++ luiz/script.js
@@ -15,16 +15,38 @@ var som = document.querySelectorAll('audio'); //todos os elementos de áudio
 
 //função que faz os audios pararem apenas se a checkbox estiver verificada (usado nos botões com áudio)
 function voltarDoZero(){
-    var checkbox_parar = document.getElementById('opcaoParar'); //checkbox de parar os sons
-    if (checkbox_parar.checked) {
-        parar()
-    }
+  var checkbox_parar = document.getElementById('opcaoParar'); //checkbox de parar os sons
+  if (checkbox_parar.checked) {
+    parar()
+  }
 };
 
 //função q faz todos os áudios pararem (usado no botão de mute)
 function parar(){
-    for (var i = 0; i < som.length; i++) {
-        som[i].pause(); //pausar o som onde ele está
-        som[i].currentTime = 0; //voltar o som do zero
-    }
+  for (var i = 0; i < som.length; i++) {
+    som[i].pause(); //pausar o som onde ele está
+    som[i].currentTime = 0; //voltar o som do zero
+  }
 };
+
+//SEU GATOOOOOOOOOO
+sfx = document.getElementById('sfx');
+const divCecato = document.getElementById('divCecato');
+
+function cecato(){
+  sfx.play();
+  divCecato.style.display = 'block';
+};
+
+const requiredKeys = ['KeyC', 'KeyE', 'KeyC', 'KeyA', 'KeyT', 'KeyO']; //as teclas que tem que apertar
+let pressedKeys = [];
+
+document.addEventListener('keydown', (event) => {
+	pressedKeys.push(event.code);
+	if (pressedKeys.length > requiredKeys.length) {
+		pressedKeys.shift();
+	} //faz a tecla pressionada ter o seu valor salvo na lista "pressedKeys" e remove as teclas antigas
+	if (pressedKeys.toString() === requiredKeys.toString() && divCecato.style.display == 'none'){ //para evitar que tenha mais de um botão
+		cecato();
+	} //cria o botão e toca o efeitinho se a condicional for cumprida
+});
